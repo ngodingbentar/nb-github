b536d105d813ebc8c60895467971c662179fa0f1
ngodingbentar <dewaqintoro@gmail.com>
2023-11-17 15:54:43 +0700

    Added: axios

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,43 @@
 'use client';
 
+import { useState } from 'react'
 import { BiSearch } from 'react-icons/bi';
 
 const Navbar = () => {
+  const [search, setSearch] = useState('')
+
+  const doSearch = async() => {
+    if (!search) {
+      return
+    } else {
+      try {
+        const res = await fetch(`https://api.github.com/search/users?q=${search}&per_page=5`)
+        const data = await res.json()
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
   return (
     <div className="navbar">
       <div className="navbar__title">
         Github Repositories Explore
       </div>
       <div className="navbar__search">
-        <input className="focus:ring-2 focus:ring-green-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-2 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="Search User..." />
+        <input
+          className="focus:ring-2 focus:ring-green-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-2 ring-1 ring-slate-200 shadow-sm"
+          type="text"
+          aria-label="Filter projects"
+          placeholder="Search User..."
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
       </div>
       <div className="navbar__btn">
         <button
-          className='flex items-center justify-center'
-          onClick={() => { }}>
+          className='flex items-center justify-center w-full'
+          onClick={doSearch}>
           <span className="mr-2">
             Search
           </span>
